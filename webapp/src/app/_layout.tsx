e5b4ea2d0bf9638083beeff2044292f6b476aa8d
thuhataplamdev <thuha140105@gmail.com>
2026-05-31 12:58:00 +0700

    UI(T-B05): Implement Register screen UI (fullname/email/pass/confirm + strength) (#162)

--- webapp/src/app/_layout.tsx
+++ webapp/src/app/_layout.tsx
@@ -1,15 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "expo-router";
-import { useColorScheme } from "react-native";
+import { DarkTheme, ThemeProvider, Stack } from "expo-router";
 
 import { AnimatedSplashOverlay } from "@/components/animated-icon";
-import AppTabs from "@/components/app-tabs";
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DarkTheme}>
       <AnimatedSplashOverlay />
-      <AppTabs />
+
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
     </ThemeProvider>
   );
 }
