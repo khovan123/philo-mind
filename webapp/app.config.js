f2f90f86d4b0b213d6e86c8f5cc5ccc2ebc69f24
NguyenDat204 <nguyentiendat2911204@gmail.com>
2026-06-06 19:17:09 +0700

    e2e smoke test

--- webapp/app.config.js
+++ webapp/app.config.js
@@ -2,10 +2,6 @@ const app = require("./app.json");
 
 const easProjectId = process.env.EAS_PROJECT_ID ?? app.expo.extra?.eas?.projectId;
 
-if (!easProjectId) {
-  throw new Error("EAS_PROJECT_ID is required for EAS Build and EAS Update");
-}
-
 module.exports = {
   ...app.expo,
   updates: easProjectId
